49f2f6aa00918aa99de611aaf0f3df9a525ae95b
ajimenez5 <ajimenez5@users.noreply.github.com>
2017-12-02 12:18:16 -0600

    started the code

---
+++ Loops.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 2, 2017
+
+@author: ITAUser
+'''
+
+number = int(input("Enter a number: ")); 
+for i in range [number]:
+    def finding_factors (x):
+        return Factor list 
+    print 
+
